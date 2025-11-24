b34abf8fc21478a9a0cd7566e584b4248748c8d5
isabellakieft <isabella.kieft@hva.nl>
2025-11-24 16:53:41 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -12,13 +12,17 @@ const hamburger = document.querySelector("header nav > button");
 const hamburgerShow = document.querySelector("header nav > ul");
 
 
+// locatieButton.onpointerenter = toggleLocaties;
 locatieButton.onclick = toggleLocaties;
+// locatieButton.onpointerleave = toggleLocaties;
 
 function toggleLocaties() {
     locatieList.classList.toggle("is-open");
 }
 
+// locatieButton2.onpointerenter = toggleLocaties2;
 locatieButton2.onclick = toggleLocaties2;
+// locatieButton2.onpointerleave = toggleLocaties2;
 
 function toggleLocaties2() {
     locatieList2.classList.toggle("open");
